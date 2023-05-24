21a296255e3d66d6bc0d54e7828ccde5d5c4c1d1
devlucas <lussivbsilva1@gmail.com>
2023-05-24 17:50:39 -0300

    Adicionando funcionalidade de senha e consertando a de email

--- cadastro.js
+++ cadastro.js
@@ -1,6 +1,14 @@
-const cadastrarNome = (email) =>{
+const cadastrarEmail = (email) =>{
   return email;
 }
 
 
 //PEGANDO EMAIL E CADASTRANDO NA PLATAFORMA.
+
+
+const cadastrarSenha = (senha) =>{
+  return senha;
+}
+
+// ALTERANDO FUNÇÃO DO EMAIL E ADICIONANDO NOVA DE SENHA
+
